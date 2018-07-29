2bfcfb3276f411921183f0b694a128069608aebd
Caleb Evans <caleb@calebevans.me>
2018-07-28 19:14:46 -0700

    Sharpen canvas for retina displays

--- app/scripts/sketcher.js
+++ app/scripts/sketcher.js
@@ -8,11 +8,13 @@ class Sketcher {
       canvas: canvas,
       styles: {
         strokeStyle: '#000',
-        lineWidth: 8,
+        lineWidth: 12,
         lineCap: 'round',
         lineJoin: 'round'
       }
     });
+    // The actual size of the canvas divided by its display size
+    this.canvasScaleFactor = 2;
     this.mousedown = false;
     this.lastX = null;
     this.lastY = null;
@@ -26,8 +28,8 @@ class Sketcher {
       // Cache computed canvas offsets for the duration of the drag
       this.canvasOffsetLeft = this.canvas.offsetLeft;
       this.canvasOffsetTop = this.canvas.offsetTop;
-      let startX = event.pageX - this.canvasOffsetLeft;
-      let startY = event.pageY - this.canvasOffsetTop;
+      let startX = (event.pageX - this.canvasOffsetLeft) * this.canvasScaleFactor;
+      let startY = (event.pageY - this.canvasOffsetTop) * this.canvasScaleFactor;
       this.frame.startNewGroup();
       this.frame.addPoint(startX, startY);
       this.lastX = startX;
@@ -37,8 +39,8 @@ class Sketcher {
     this.canvas.addEventListener('mousemove', (event) => {
       event.preventDefault();
       if (this.mousedown) {
-        let endX = event.pageX - this.canvasOffsetLeft;
-        let endY = event.pageY - this.canvasOffsetTop;
+        let endX = (event.pageX - this.canvasOffsetLeft) * this.canvasScaleFactor;
+        let endY = (event.pageY - this.canvasOffsetTop) * this.canvasScaleFactor;
         let diffX = endX - this.lastX;
         let diffY = endY - this.lastY;
         if (diffX !== 0 || diffY !== 0) {
