731b78011e93b79945f629b525eccb33f2c06f50
Caleb Evans <caleb@calebevans.me>
2018-07-28 20:51:07 -0700

    Modularize mouse event logic

--- app/scripts/sketcher.js
+++ app/scripts/sketcher.js
@@ -24,41 +24,53 @@ class Sketcher {
   bindMouseEvents() {
     this.canvas.addEventListener('mousedown', (event) => {
       event.preventDefault();
-      if (this.interactivityEnabled) {
-        this.mousedown = true;
-        // Cache computed canvas offsets for the duration of the drag
-        this.canvasOffsetLeft = this.canvas.offsetLeft;
-        this.canvasOffsetTop = this.canvas.offsetTop;
-        let startX = (event.pageX - this.canvasOffsetLeft) * this.canvasScaleFactor;
-        let startY = (event.pageY - this.canvasOffsetTop) * this.canvasScaleFactor;
-        this.frame.startNewGroup();
-        this.frame.addPoint(startX, startY);
-        this.lastX = startX;
-        this.lastY = startY;
-        this.frame.render();
-      }
+      this.handleMousedown(event);
     });
     this.canvas.addEventListener('mousemove', (event) => {
       event.preventDefault();
-      if (this.mousedown && this.interactivityEnabled) {
-        let endX = (event.pageX - this.canvasOffsetLeft) * this.canvasScaleFactor;
-        let endY = (event.pageY - this.canvasOffsetTop) * this.canvasScaleFactor;
-        let diffX = endX - this.lastX;
-        let diffY = endY - this.lastY;
-        if (diffX !== 0 || diffY !== 0) {
-          this.frame.addPoint(diffX, diffY);
-          this.lastX = endX;
-          this.lastY = endY;
-          this.frame.render();
-        }
-      }
+      this.handleMousemove(event);
     });
     this.canvas.addEventListener('mouseup', (event) => {
-      if (this.interactivityEnabled) {
-        event.preventDefault();
-        this.mousedown = false;
-      }
+      event.preventDefault();
+      this.handleMouseup(event);
     });
+  }
+
+  handleMousedown(event) {
+    if (this.interactivityEnabled) {
+      this.mousedown = true;
+      // Cache computed canvas offsets for the duration of the drag
+      this.canvasOffsetLeft = this.canvas.offsetLeft;
+      this.canvasOffsetTop = this.canvas.offsetTop;
+      let startX = (event.pageX - this.canvasOffsetLeft) * this.canvasScaleFactor;
+      let startY = (event.pageY - this.canvasOffsetTop) * this.canvasScaleFactor;
+      this.frame.startNewGroup();
+      this.frame.addPoint(startX, startY);
+      this.lastX = startX;
+      this.lastY = startY;
+      this.frame.render();
+    }
+  }
+
+  handleMousemove(event) {
+    if (this.mousedown && this.interactivityEnabled) {
+      let endX = (event.pageX - this.canvasOffsetLeft) * this.canvasScaleFactor;
+      let endY = (event.pageY - this.canvasOffsetTop) * this.canvasScaleFactor;
+      let diffX = endX - this.lastX;
+      let diffY = endY - this.lastY;
+      if (diffX !== 0 || diffY !== 0) {
+        this.frame.addPoint(diffX, diffY);
+        this.lastX = endX;
+        this.lastY = endY;
+        this.frame.render();
+      }
+    }
+  }
+
+  handleMouseup() {
+    if (this.interactivityEnabled) {
+      this.mousedown = false;
+    }
   }
 
 }
