4fd60feae6ffa6f8a4bbe055340feee63b9ac790
Caleb Evans <caleb@calebevans.me>
2018-08-03 21:07:46 -0700

    Register service worker

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,3 +3,9 @@ import StoryEditor from './story-editor.js';
 StoryEditor.restore({
   editorElement: document.querySelector('.story-editor')
 });
+
+window.addEventListener('load', () => {
+  if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('service-worker.js');
+  }
+});
