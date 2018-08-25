6f34f915cb1b204d26373c0068a4dadf0b2b1a01
Caleb Evans <caleb@calebevans.me>
2018-08-25 14:26:45 -0700

    Allow left and right arrow keys to navigate frames

--- app/scripts/components/story.js
+++ app/scripts/components/story.js
@@ -3,8 +3,20 @@ import StoryEditorComponent from './story-editor.js';
 
 class StoryComponent {
 
+  navigateFramesViaKeyboard(story, key) {
+    if (key === 'ArrowLeft') {
+      story.selectPreviousFrame();
+    } else if (key === 'ArrowRight') {
+      story.selectNextFrame();
+    }
+  }
+
   view({attrs: {app, story}}) {
-    return m('div.story', [
+    return m('div.story[tabindex=-1]', {
+      oncreate: ({dom}) => dom.focus(),
+      onupdate: ({dom}) => dom.focus(),
+      onkeydown: ({key}) => this.navigateFramesViaKeyboard(story, key)
+    }, [
       m(StoryHeaderComponent, {app, story}),
       m(StoryEditorComponent, {story})
     ]);
