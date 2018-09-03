caleb531/flip-book

commit d22ca2c1cd417d3ed43a0c0d0a2103ad611bc3ea

WIP: begin writing architecture for per-group styles
This will pave the way for a user-facing Stroke Width setting.

app/scripts/models/frame.js
@@ -5,12 +5,10 @@ class Frame {
   constructor({styles = {}, groups = [], undoHistory = []} = {}) {
     this.temporaryId = Frame.nextAutoIncrementedId;
     Frame.nextAutoIncrementedId += 1;
-    this.styles = Object.assign({
-      strokeStyle: '#000',
-      lineWidth: 12,
-      lineCap: 'round',
-      lineJoin: 'round'
-    }, styles);
+    this.styles = Object.assign({}, Frame.defaultStyles, _.pick(styles, [
+      'strokeStyle',
+      'lineWidth'
+    ]));
     this.groups = groups;
     this.undoHistory = undoHistory;
   }
@@ -46,7 +44,6 @@ class Frame {
     if (backgroundColor) {
       this.setBackground(ctx, backgroundColor);
     }
-    this.setCanvasStyles(ctx);
     this.drawGroups(ctx);
   }
 
@@ -65,19 +62,30 @@ class Frame {
     ctx.fillStyle = 'transparent';
   }
 
-  setCanvasStyles(ctx) {
-    for (let styleKey in this.styles) {
-      if (Object.prototype.hasOwnProperty.call(this.styles, styleKey)) {
-        ctx[styleKey] = this.styles[styleKey];
-      }
+  setGlobalStyles(ctx) {
+    ctx.lineCap = this.styles.lineCap;
+    ctx.lineJoin = this.styles.lineJoin;
+  }
+
+  setGroupStyles(ctx, group) {
+    this.setGroupStyle(ctx, group, 'strokeStyle');
+    this.setGroupStyle(ctx, group, 'lineWidth');
+  }
+
+  setGroupStyle(ctx, group, styleName) {
+    let styleValue = group.styles ? group.styles.strokeStyle : this.styles.strokeStyle;
+    if (styleValue !== ctx[styleName]) {
+      ctx[styleName] = styleValue;
     }
   }
 
   drawGroups(ctx) {
+    this.setGlobalStyles(ctx);
     for (let g = 0; g < this.groups.length; g += 1) {
       let group = this.groups[g];
       let currentX = group.points[0][0];
       let currentY = group.points[0][1];
+      this.setGroupStyles(ctx, group);
       if (group.points.length === 1) {
         // Draw a circle
         ctx.fillStyle = this.styles.strokeStyle;
@@ -118,5 +126,11 @@ class Frame {
 
 }
 Frame.nextAutoIncrementedId = 0;
+Frame.defaultStyles = {
+  strokeStyle: '#000',
+  lineWidth: 1,
+  lineCap: 'round',
+  lineJoin: 'round'
+};
 
 export default Frame;

commit b710efb3d89df6d39a758a947230f42e7c4ea479

Fix bug where lineWidth was not being set on ctx
The bug in question was caused by a logic error introduced in d22ca2c.

app/scripts/models/frame.js
@@ -73,7 +73,7 @@ class Frame {
   }
 
   setGroupStyle(ctx, group, styleName) {
-    let styleValue = group.styles ? group.styles.strokeStyle : this.styles.strokeStyle;
+    let styleValue = group.styles ? group.styles[styleName] : this.styles[styleName];
     if (styleValue !== ctx[styleName]) {
       ctx[styleName] = styleValue;
     }