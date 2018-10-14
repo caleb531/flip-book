8b1051f89566ded2be3f48b59187b71519d3d5f9
Caleb Evans <caleb@calebevans.me>
2018-10-14 14:02:58 -0700

    Move templatedUrls next to globPatterns
    It makes more sense for them to be placed next to each other.

--- gulpfile.js
+++ gulpfile.js
@@ -68,15 +68,15 @@ gulp.task('sw', () => {
     globPatterns: [
       '**\/*.{js,css,svg,png}'
     ],
-    swSrc: 'app/scripts/service-worker.js',
-    swDest: 'public/service-worker.js',
     // Precaching index.html using templatedUrls fixes a "Response served by
     // service worker has redirections" error on iOS 12; see
     // <https://github.com/v8/v8.dev/issues/4> and
     // <https://github.com/v8/v8.dev/pull/7>
     templatedUrls: {
       '/': ['index.html']
-    }
+    },
+    swSrc: 'app/scripts/service-worker.js',
+    swDest: 'public/service-worker.js'
   }).then(({warnings}) => {
     warnings.forEach(console.warn);
   });
