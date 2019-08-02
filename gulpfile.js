d0fcda926044b6f71248cecec5d75d4a2a54118d
Caleb Evans <caleb@calebevans.me>
2019-08-01 19:06:12 -0700

    Update Workbox to v4.3.1

--- gulpfile.js
+++ gulpfile.js
@@ -81,11 +81,11 @@ gulp.task('sw', () => {
       '**\/*.{js,css,png}',
       'icons/*.svg'
     ],
-    // Precaching index.html using templatedUrls fixes a "Response served by
+    // Precaching index.html using templatedURLs fixes a "Response served by
     // service worker has redirections" error on iOS 12; see
     // <https://github.com/v8/v8.dev/issues/4> and
     // <https://github.com/v8/v8.dev/pull/7>
-    templatedUrls: {
+    templatedURLs: {
       // '.' must be used instead of '/' because the app is not served from the
       // root of the domain (but rather, from a subdirectory)
       '.': ['index.html']
