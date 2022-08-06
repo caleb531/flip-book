a3b00487e767f31fad5f1aa4614a96120c992d92
Caleb Evans <caleb@calebevans.me>
2022-08-06 14:31:37 -0700

    Rename setupJest to setup-jest
    For consistency with the project's filename convention.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 export default {
   testEnvironment: 'jsdom',
   preset: 'rollup-jest',
-  setupFiles: ['<rootDir>/test/setupJest.js'],
+  setupFiles: ['<rootDir>/test/setup-jest.js'],
   // Display coverage summary below file-by-file coverage breakdown
   coverageReporters: ['clover', 'json', 'lcov', 'html', 'text', 'text-summary']
 };
