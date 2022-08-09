3b0a6a076bb68c8bec8559ea2d05c1e90d4f6928
Caleb Evans <caleb@calebevans.me>
2022-08-08 17:25:23 -0700

    Prefer setupFilesAfterEnv in Jest config
    This will allow us to run test fixtures (e.g. beforeEach and afterEach)
    in our setup file.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 export default {
   testEnvironment: 'jsdom',
   preset: 'rollup-jest',
-  setupFiles: ['<rootDir>/test/setup-jest.js'],
+  setupFilesAfterEnv: ['<rootDir>/test/setup-jest.js'],
   // Display coverage summary below file-by-file coverage breakdown
   coverageReporters: ['clover', 'json', 'lcov', 'html', 'text', 'text-summary']
 };
