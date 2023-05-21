0df4fc3230d5db28e28f26ef6457865b84950d81
Caleb Evans <caleb@calebevans.me>
2023-05-21 16:30:20 -0700

    Upgrade to @fontsource v5
    Also switch to the default font-display:swap, since it does not seem to
    have any adverse effect.

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 import m from 'mithril';
 import AppComponent from './components/app.js';
 import '../styles/index.scss';
+import '@fontsource/roboto-slab/400.css';
+import '@fontsource/roboto-slab/600.css';
 
 m.mount(document.querySelector('main'), AppComponent);
