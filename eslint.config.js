a045ffe0e512087fd220ce4c48dcda6bc6e419ba
Caleb Evans <caleb@calebevans.me>
2024-08-17 18:12:56 -0700

    Upgrade eslint to v9 with flat config

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import vitest from "@vitest/eslint-plugin";
+import js from '@eslint/js';
+import globals from 'globals';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off'
+    }
+  },
+  {
+    files: ["test/*.js"],
+    languageOptions: {
+      globals: {
+        ...vitest.environments.env.globals
+      }
+    }
+  },
+  {
+    ignores: [
+      '.DS_Store',
+      'node_modules',
+      'build',
+      'dist',
+      'coverage',
+      '.vercel',
+      '.env',
+      '.env.*',
+      '!.env.example',
+      'pnpm-lock.yaml',
+      'package-lock.json',
+      'yarn.lock'
+    ]
+  }
+];
