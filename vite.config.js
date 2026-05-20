ad80aac24a7ce5daecddf2bc545a541eb0e5f2d7
Caleb Evans <caleb@calebevans.me>
2026-05-20 15:46:43 -0700

    Upgrade dependencies to latest, including Vite 8

--- vite.config.js
+++ vite.config.js
@@ -9,13 +9,16 @@ export default defineConfig({
   // specify . as the base directory to serve from
   base: './',
   // Enable JSX processing
-  esbuild: {
+  oxc: {
     // We need to use _m as the imported name so that it doesn't collide with
     // explicitly importing _m, while still allowing us to have organizeImports
     // strip out "unused" mithril imports
     jsxInject: "import _m from 'mithril'",
-    jsxFactory: '_m',
-    jsxFragment: '_m.Fragment'
+    jsx: {
+      runtime: 'classic',
+      pragma: '_m',
+      pragmaFrag: '_m.Fragment'
+    }
   },
   plugins: [
     VitePWA({
